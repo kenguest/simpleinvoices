af390327ffa33889ef9a630a8a14448f7f1f858b
Justin Kelly <justin@kelly.org.au>
2007-11-29 13:22:09 +0000

    website update

--- website/src/MarketPlace.php
+++ website/src/MarketPlace.php
@@ -4,32 +4,57 @@
 
 <p class=CParagraph>
 <br>
-The Simple Invoices Market Place is still in Beta 
+The Simple Invoices Market Place enables you to easily find a 
 <h4 class=CHeading>SixHQ :: <a href="http://www.sixhq.com">http://www.sixhq.com</a></h4>
+<div style="float:right;">
+	<a href="http://www.sixhq.com" title="SixHQ - Simple Invoices Online Hosting"><img class="logo" alt="SixHQ - Simple Invoices Online Hosting" src="/files/sixhq_logo.png"/></a>
+</div>
 <p class=CParagraph>
-SixHQ was started by Justin Kelly, the founder and still one of the current developers of Simple Invoices.
+SixHQ was started by Justin Kelly, the founder and still one of the current developers of Simple Invoices. The aim of SixHQ is to provide services to enhance your Simple Invoices experience. This ranges from your own dedicated server for Simple Invoices to modifcations and support.
 <br><br>
 Available services:
 <ul>
 	<li>
-		Simple Invoices hosting	
+		<a href="http://www.sixhq.com/hosting" title="Simple Invoices hosting">
+			Simple Invoices hosting
+		</a>
 	</li>
 	<li>
+		<a href="http://www.sixhq.com/modifications" title="Simple Invoices modications">
 		Modifications	
+		</a>
 	</li>
 	<li>
+		<a href="http://www.sixhq.com/custom_reports" title="Simple Invoices custom_reports">
 		Custom Reports
+		</a>
 	</li>
 	<li>
+		<a href="http://www.sixhq.com/support" title="Simple Invoices support">
 		Support
+		</a>
 	</li>
 	<li>
+		<a href="http://www.sixhq.com/installation" title="Simple Invoices installation">
 		Installation
+		</a>
+	</li>
+	<li>
+		<a href="http://www.sixhq.com/freelance" title="Freelance web development">
+		Freelance web development
+		</a>
 	</li>
 </ul>
+<p class=CParagraph>
+If you have any queries about how SixHQ can help you please don't hesitate to drop me a line ( justin@sixhq.com ).  
+<br><br>
+Whatever your Simple Invoices needs are we can sort it out.
+
+</p>
 <!-- TEXT HERE -->
 <br>
 <br>
+<h4 class=CHeading>How to join</h4>
 <p class=CParagraph>
 If you would like your company listed here please contact Justin Kelly on justin@kelly.org.au
 </p>
