350dc1e508b6570e3e052872e439594681e556a9
Justin Kelly <justin@kelly.org.au>
2007-11-30 04:17:04 +0000

    minor website update

--- website/src/MarketPlace.php
+++ website/src/MarketPlace.php
@@ -4,15 +4,17 @@
 
 <p class=CParagraph>
 <br>
-The Simple Invoices Market Place enables you to easily find a 
+The Simple Invoices Market Place enables you to easily find a Simple Invoices service provider.
 <h4 class=CHeading>SixHQ :: <a href="http://www.sixhq.com">http://www.sixhq.com</a></h4>
 <div style="float:right;">
 	<a href="http://www.sixhq.com" title="SixHQ - Simple Invoices Online Hosting"><img class="logo" alt="SixHQ - Simple Invoices Online Hosting" src="/files/sixhq_logo.png"/></a>
 </div>
 <p class=CParagraph>
-SixHQ was started by Justin Kelly, the founder and still one of the current developers of Simple Invoices. The aim of SixHQ is to provide services to enhance your Simple Invoices experience. This ranges from your own dedicated server for Simple Invoices to modifcations and support.
+SixHQ was started by Justin Kelly, the founder and still one of the current developers of Simple Invoices. 
 <br><br>
-Available services:
+The aim of SixHQ is to provide services to enhance your Simple Invoices experience. This ranges from your own dedicated server for Simple Invoices to modifcations and support.
+<br><br>
+Available Simple Invoices services:
 <ul>
 	<li>
 		<a href="http://www.sixhq.com/hosting" title="Simple Invoices hosting">
@@ -39,6 +41,10 @@ Available services:
 		Installation
 		</a>
 	</li>
+</ul>
+
+<ul>Other services:
+
 	<li>
 		<a href="http://www.sixhq.com/freelance" title="Freelance web development">
 		Freelance web development
@@ -53,10 +59,20 @@ Whatever your Simple Invoices needs are we can sort it out.
 </p>
 <!-- TEXT HERE -->
 <br>
+
+<h4 class=CHeading>Why a Market Place?</h4>
+<p class=CParagraph>
+Though the Simple Invoices Forums rock and provide the level of support that most people will need, it became clear that some members of the Simple Invoices community required a level of support that was not practical under the normal open source/community model
+<br><br>
+What also became evident was a need to provide a clear path for development of extensions or modifications and other services for Simple Invoices for specific people on a for $ basis 
+<br><br>
+For more on why the Market Place was started refer: <a href="http://www.sixhq.com/#why" title="Simple Invoices eXtended HQ">http://www.sixhq.com/#why</a>
+</p>
 <br>
+
 <h4 class=CHeading>How to join</h4>
 <p class=CParagraph>
-If you would like your company listed here please contact Justin Kelly on justin@kelly.org.au
+If you would like your company listed here as a Simple Invoices service provider please contact Justin Kelly on justin@kelly.org.au
 </p>
 </div></div>
 
