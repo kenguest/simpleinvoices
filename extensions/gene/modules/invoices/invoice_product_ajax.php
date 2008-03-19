8030977c82084664e674c9389da71ea70d908f21
Justin Kelly <justin@kelly.org.au>
2008-03-19 10:48:25 +0000

    invoice ajax file - using mysqlQuery now and config.php now

--- extensions/gene/modules/invoices/invoice_product_ajax.php
+++ extensions/gene/modules/invoices/invoice_product_ajax.php
@@ -1,13 +1,20 @@
 <?php
 // Connecting, selecting database
-$db = mysql_connect('localhost', 'root', '') or die('Could not connect: ' . mysql_error());
-mysql_select_db('simple_invoices') or die('Could not select database');
+//$db = mysql_connect('localhost', 'root', '') or die('Could not connect: ' . mysql_error());
+//mysql_select_db('simple_invoices') or die('Could not select database');
 
+include('../../../../config/config.php');
+include('../../../../include/sql_queries.php');
+//include('../../../../include/functions.php');
+
+//include_once('../../../../include/include_main.php');
+
+ 
 if($_GET['id'])
 {
 	sleep(2);
 	$sql = sprintf('SELECT * FROM si_products WHERE id = %d LIMIT 1', $_GET['id']);
-	$states = mysql_query($sql) or die('Query Failed:' . mysql_error());
+	$states = mysqlQuery($sql, $conn) or die('Query Failed:' . mysql_error());
 	$output = '';
 	if(mysql_num_rows($states) > 0)
 	{	
@@ -26,7 +33,7 @@ if($_GET['id'])
 	echo $output;
 	
 	mysql_free_result($states);
-	mysql_close($db);
+	//mysql_close($db);
 	exit();
 } else {
 
@@ -35,8 +42,7 @@ echo "";
 
 // Perform teh Queries!
 $sql = 'SELECT * FROM si_products';
-$country = mysql_query($sql) or die('Query Failed:' . mysql_error());
+$country = mysqlQuery($sql) or die('Query Failed:' . mysql_error());
 
 
 ?>
-
