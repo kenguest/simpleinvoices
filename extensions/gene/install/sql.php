ea349361c949bac34e3fe7d8ada3de7a11071889
Justin Kelly <justin@kelly.org.au>
2008-03-25 07:38:24 +0000

    minor extension

--- extensions/gene/install/sql.php
+++ extensions/gene/install/sql.php
@@ -16,4 +16,13 @@
 	}
 	$patch['gene']['2']['date'] = "20080321";
 	//ALTER TABLE `si_products` ADD `unit_cost` DOUBLE( 25, 6 ) NULL AFTER `unit_price` ;
+
+
+	$patch['gene']['3']['name'] = "Add unit_load_cost to invoice items table";
+	$patch['gene']['3']['patch'] = "ALTER TABLE si_invoice_items ADD unit_load_cost DECIMAL( 25, 6 ) NOT NULL AFTER unit_cost ;";
+    if ($db_server == "pgsql") {
+		$patch['gene']['3']['patch'] = "ALTER TABLE ".TB_PREFIX."invoice_items ADD unit_load_cost DECIMAL( 25, 6 ) NOT NULL AFTER unit_cost ;";
+	}
+	$patch['gene']['2']['date'] = "20080325";
+	//ALTER TABLE `si_invoice_items` ADD `unit_load_cost` DECIMAL( 25, 6 ) NOT NULL AFTER `unit_cost` ;
 ?>
