d5755db52a39b90c46586d7edd97b5668c96cfc4
Justin Kelly <justin@kelly.org.au>
2008-03-28 03:23:51 +0000

    gene extensions: customer or vendor flag

---
+++ extensions/gene/modules/customers/add.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* Script: add.php
+* 	Customers add page
+*
+* Authors:
+*	 Justin Kelly, Nicolas Ruflin
+*
+* Last edited:
+* 	 2007-07-19
+*
+* License:
+*	 GPL v2 or above
+*
+* Website:
+* 	http://www.simpleinvoices.org
+ */
+
+//stop the direct browsing to this file - let index.php handle which files get displayed
+checkLogin();
+
+$customFieldLabel = getCustomFieldLabels();
+
+//if valid then do save
+if ($_POST['name'] != "" ) {
+	include("./modules/customers/save.php");
+}
+$pageActive = "customers";
+$smarty->assign('pageActive', $pageActive);
+$smarty -> assign('customFieldLabel',$customFieldLabel);
+
+$smarty->assign('customerOrVendor', array('Customer','Vendor'));
+
+?>
