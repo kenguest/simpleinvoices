788c71baa7caa637d6642f2c0364135de15ebec2
Justin Kelly <justin@kelly.org.au>
2008-04-19 12:11:30 +0000

    school extension - start course section

---
+++ extensions/school/modules/course/manage.php
@@ -0,0 +1,15 @@
+<?php
+
+//stop the direct browsing to this file - let index.php handle which files get displayed
+checkLogin();
+
+$products = getProducts();
+
+$pageActive = "products";
+
+$smarty->assign('pageActive', $pageActive);
+$smarty -> assign("products",$products);
+
+getRicoLiveGrid("rico_product","{ type:'number', decPlaces:0, ClassName:'alignleft' },,{ type:'number', decPlaces:2, ClassName:'alignleft' }");
+
+?>
