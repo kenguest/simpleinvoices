b34ee9684d9b9746998b79d2f5b7d488682416ae
Justin Kelly <justin@kelly.org.au>
2008-06-22 06:04:06 +0000

    school - minor

--- include/jquery/jquery.datePicker.conf.js
+++ include/jquery/jquery.datePicker.conf.js
@@ -1,7 +1,7 @@
 $(document).ready(init);
 function init()
 {
-$.datePicker.setDateFormat('ymd','-');
-$('input#date1').datePicker({startDate:'01/01/1970'});
+	$.datePicker.setDateFormat('ymd','-');
+	$('input.date-picker').datePicker({startDate:'01/01/1970'});
 }
 
