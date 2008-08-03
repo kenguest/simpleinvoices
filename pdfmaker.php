3a9f152d9e23500cf5d41af6c6b126c4104b3b0e
Justin Kelly <justin@kelly.org.au>
2008-08-03 07:52:45 +0000

    trunk prod matrix update

--- pdfmaker.php
+++ pdfmaker.php
@@ -6,7 +6,6 @@
 
 // This file must be in the root of the application folder where the index.php resides
 
-require_once("./config/config.php");		// for pdf variables
 require_once("./include/init.php");	// for getInvoice() and getPreference()
 
 // $defaults = getSystemDefaults(); // Not required as of now.
