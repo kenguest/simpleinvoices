132146d933a25cb6c27eb96ee371515af606f935
Justin Kelly <justin@kelly.org.au>
2011-09-19 04:15:41 +0000

    minor ach work

--- templates/invoices/default/plugins/function.online_payment_link.php
+++ templates/invoices/default/plugins/function.online_payment_link.php
@@ -49,9 +49,17 @@ function smarty_function_online_payment_link($params, &$smarty) {
     if (in_array("ach",explode(",", $params['type'])))
     {
 
-       $xinvoice_id = simple_encrypt($params['invoice']);
+        $today = date('Y-m-d');
+        $datetime1 = new DateTime('0001-01-01');
+        $datetime2 = new DateTime($today);
+        $interval = $datetime1->diff($datetime2);
+        $seconds =  $interval->format('%a') * 24 * 60 * 60;
+        //get biller secure trans key here
+        $hash = hash_hmac('md5',"APILOGINID | 10 | 1.0 | 5.00 | 634094514514687490 | 100055", "SECURETRANSACTIONKEY") ;
+
         $link = "<a 
-            href=\"$siUrl/index.php?module=payments&view=ach&id=".$xinvoice_id."\">";
+            href=\"https://swp.paymentsgateway.net/default.aspx?pg_api_login_id=".urlencode($params['business'])."&pg_billto_postal_name_first=".$hah."&pg_billto_postal_name_last=".$hash."&pg_transaction_type=".$hash."&pg_version_number=".$hash."&pg_total_amount=".urlencode(number_format($params['amount'], 2, '.', ''))."&pg_utc_time=".$seconds."&pg_transaction_order_number=".$hash."&pg_ts_hash=".$hash."aaitem_name=".urlencode($params['item_name'])."&invoice=".urlencode($params['invoice'])."&amount=".urlencode(number_format($params['amount'], 2, '.', ''))."&currency_code=".urlencode($params['currency_code'])."&notify_url=".urlencode($params['notify_url'])."&return=".urlencode($params['return_url'])."&no_shipping=1&no_note=1&custom=domain_id:".urlencode($domain_id)."; \">";
+
         if($params['include_image'] == "true")
         {
             $link .= "<img border='0' src='".urlsafe($url)."/images/common/pay_with_ach.gif'/>";
