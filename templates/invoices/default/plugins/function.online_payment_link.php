39d2be97c4ceaf100153db51ca54fa08ca0ce3dd
Justin Kelly <justin@kelly.org.au>
2011-09-19 14:16:31 +0000

    more ach fix

--- templates/invoices/default/plugins/function.online_payment_link.php
+++ templates/invoices/default/plugins/function.online_payment_link.php
@@ -71,9 +71,7 @@ function smarty_function_online_payment_link($params, &$smarty) {
             ."&pg_billto_postal_stateprov=". urlencode($params['customer']['state'])
             ."&pg_billto_postal_postalcode=". $params['customer']['zip_code']
             ."&pg_consumerorderid=". $params['invoice']
-            ."&pg_return_url=". urlencode($siUrl.'/api-ach/') ."&pg_save_client=2". 
-            urlencode($params['return_url']). "&no_shipping=1&no_note=1&custom=domain_id:". 
-            urlencode($domain_id). "'>";
+            ."&pg_return_url=". $siUrl. "/api-ach/&pg_save_client=2'>";
 
         if($params['include_image'] == "true")
         {
